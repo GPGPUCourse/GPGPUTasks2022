35098dd766843ade76ce68151dc545b4c38493d0
Raed Romanov <raid_r@mail.ru>
2022-10-08 13:43:23 +0300

    Task 4.2 done

--- src/cl/matrix_multiplication.cl
+++ src/cl/matrix_multiplication.cl
@@ -1,4 +1,108 @@
-__kernel void matrix_multiplication(...)
-{
-    // TODO
+typedef struct Mtx {
+    __global float* a;
+    size_t m;
+    size_t n;
+} Mtx;
+
+inline float mtx_get(Mtx m, uint i, uint j) {
+    return m.a[j * m.n + i];
+}
+
+inline void mtx_set(Mtx m, uint i, uint j, float v) {
+    m.a[j * m.n + i] = v;
+}
+
+inline Mtx mtx_create(__global float* a, size_t m, size_t n) {
+    Mtx res;
+    res.a = a;
+    res.m = m;
+    res.n = n;
+    return res;
+}
+
+
+__kernel void matrix_multiplication(__global float* A, __global float* B, __global float* C,
+    uint M, uint K, uint N) {
+    size_t i = get_global_id(0);
+    size_t j = get_global_id(1);
+
+    if (i >= M || j >= N)
+        return;
+
+    size_t li = get_local_id(0);
+    size_t lj = get_local_id(1);
+
+    size_t tileSize = get_local_size(0);
+    size_t numTiles = K / tileSize;
+
+    Mtx a = mtx_create(A, M, K);
+    Mtx b = mtx_create(B, K, N);
+    Mtx c = mtx_create(C, M, N);
+
+    __local float tileA[17][17];
+    __local float tileB[17][17];
+
+    float sum = 0.0f;
+
+    for (size_t t = 0; t < numTiles; ++t) {
+        tileA[lj][li] = mtx_get(a, t * tileSize + li, j);
+        tileB[li][lj] = mtx_get(b, i, t * tileSize + lj);
+
+        barrier(CLK_LOCAL_MEM_FENCE);
+
+        for (size_t l = 0; l < tileSize; ++l) {
+            sum += tileA[lj][l] * tileB[li][l];
+        }
+
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    mtx_set(c, i, j, sum);
+}
+
+
+__kernel void matrix_multiplication2(__global float* A, __global float* B, __global float* C,
+    uint M, uint K, uint N) {
+    size_t i = get_global_id(0); // 0..n
+    size_t j = get_global_id(1); // 0..m/stripeSize
+    size_t li = get_local_id(0); // 0..tileSize
+    size_t lj = get_local_id(1); // 0..tileSize/stripeSize
+
+    size_t tileSize = get_local_size(0);
+    size_t numTileStripes = get_local_size(1);
+    size_t stripeSize = tileSize / numTileStripes;
+    size_t numTiles = K / tileSize;
+
+    Mtx a = mtx_create(A, M, K);
+    Mtx b = mtx_create(B, K, N);
+    Mtx c = mtx_create(C, M, N);
+
+    __local float tileA[33][33];
+    __local float tileB[33][33];
+    float sum[16];
+    for (size_t w = 0; w < stripeSize; ++w) {
+        sum[w] = 0.0f;
+    }
+
+    for (size_t t = 0; t < numTiles; ++t) {
+        for (size_t w = 0; w < stripeSize; ++w) {
+            tileA[lj * stripeSize + w][li] = mtx_get(a, t * tileSize + li, j * stripeSize + w);
+            tileB[li][lj * stripeSize + w] = mtx_get(b, i, t * tileSize + lj * stripeSize + w);
+        }
+
+        barrier(CLK_LOCAL_MEM_FENCE);
+
+        for (size_t l = 0; l < tileSize; ++l) {
+            float tmp = tileB[li][l];
+            for (size_t w = 0; w < stripeSize; ++w) {
+                sum[w] += tileA[lj * stripeSize + w][l] * tmp;
+            }
+        }
+
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    for (size_t w = 0; w < stripeSize; ++w) {
+        mtx_set(c, i, j * stripeSize + w, sum[w]);
+    }
 }
--- src/main_matrix_multiplication.cpp
+++ src/main_matrix_multiplication.cpp
@@ -19,15 +19,17 @@ int main(int argc, char **argv)
     context.init(device.device_id_opencl);
     context.activate();
 
-    int benchmarkingIters = 10; // TODO пока тестируетесь удобно выставить единицу
+    int benchmarkingIters = 10;
+    int cpuBenchmarkingIters = 5;
     unsigned int M = 1024;
-    unsigned int K = 1024;
-    unsigned int N = 1024;
-    const size_t gflops = ((size_t) M * K * N * 2) / (1000 * 1000 * 1000); // умножить на два, т.к. операция сложения и умножения
+    unsigned int K = 512;
+    unsigned int N = 2 * 1024;
+    const size_t gflop = ((size_t) M * K * N * 2) / (1000 * 1000 * 1000); // умножить на два, т.к. операция сложения и умножения
 
     std::vector<float> as(M*K, 0);
     std::vector<float> bs(K*N, 0);
     std::vector<float> cs(M*N, 0);
+    std::vector<float> cs2(M*N, 0);
 
     FastRandom r(M+K+N);
     for (unsigned int i = 0; i < as.size(); ++i) {
@@ -40,25 +42,24 @@ int main(int argc, char **argv)
 
     {
         timer t;
-        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+        for (int iter = 0; iter < cpuBenchmarkingIters; ++iter) {
             for (int j = 0; j < M; ++j) {
                 for (int i = 0; i < N; ++i) {
                     float sum = 0.0f;
                     for (int k = 0; k < K; ++k) {
-                        sum += as.data()[j * K + k] * bs.data()[k * N + i];
+                        sum += as[j * K + k] * bs[k * N + i];
                     }
-                    cs.data()[j * N + i] = sum;
+                    cs[j * N + i] = sum;
                 }
             }
             t.nextLap();
         }
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU: " << gflops / t.lapAvg() << " GFlops" << std::endl;
+        std::cout << "CPU: " << gflop / t.lapAvg() << " GFlops" << std::endl;
     }
 
     const std::vector<float> cs_cpu_reference = cs;
 
-    /*
     gpu::gpu_mem_32f as_gpu, bs_gpu, cs_gpu;
     as_gpu.resizeN(M*K);
     bs_gpu.resizeN(K*N);
@@ -71,38 +72,68 @@ int main(int argc, char **argv)
     matrix_multiplication_kernel.compile();
 
     {
-        timer t;
-        for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
-            matrix_multiplication_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, bs_gpu, cs_gpu, M, K, N);
+      timer t;
+      for (int iter = 0; iter < benchmarkingIters; ++iter) {
+        matrix_multiplication_kernel.exec(gpu::WorkSize(16, 16, N, M), as_gpu, bs_gpu, cs_gpu, M, K, N);
 
-            t.nextLap();
-        }
-        std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << gflops / t.lapAvg() << " GFlops" << std::endl;
+        t.nextLap();
+      }
+      std::cout << "First kernel:\n";
+      std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+      std::cout << "GPU: " << gflop / t.lapAvg() << " GFlops\n" << std::endl;
     }
 
     cs_gpu.readN(cs.data(), M*N);
-    */
+
+    ocl::Kernel matrix_multiplication2_kernel(matrix_multiplication, matrix_multiplication_length, "matrix_multiplication2");
+    matrix_multiplication2_kernel.compile();
+    const unsigned int tileSize = 32;
+    const unsigned int stripeSize = 8;
+
+    {
+      timer t;
+      for (int iter = 0; iter < benchmarkingIters; ++iter) {
+        matrix_multiplication2_kernel.exec(gpu::WorkSize(tileSize, tileSize / stripeSize, N, M / stripeSize), as_gpu, bs_gpu, cs_gpu, M, K, N);
+
+        t.nextLap();
+      }
+      std::cout << "Second kernel:\n";
+      std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+      std::cout << "GPU: " << gflop / t.lapAvg() << " GFlops\n" << std::endl;
+    }
+
+    cs_gpu.readN(cs2.data(), M*N);
 
     // Проверяем корректность результатов
     double diff_sum = 0;
+    double diff2_sum = 0;
     for (int i = 0; i < M * N; ++i) {
         double a = cs[i];
+        double a2 = cs2[i];
         double b = cs_cpu_reference[i];
         if (a != 0.0 && b != 0.0) {
-            double diff = fabs(a - b) / std::max(fabs(a), fabs(b));
-            diff_sum += diff;
+          double diff = fabs(a - b) / std::max(fabs(a), fabs(b));
+          diff_sum += diff;
+
+          diff = fabs(a2 - b) / std::max(fabs(a2), fabs(b));
+          diff2_sum += diff;
         }
     }
 
+    std::cout << "First kernel:\n";
     double diff_avg = diff_sum / (M * N);
     std::cout << "Average difference: " << diff_avg * 100.0 << "%" << std::endl;
     if (diff_avg > 0.01) {
         std::cerr << "Too big difference!" << std::endl;
         return 1;
+    }
+
+    std::cout << "Second kernel:\n";
+    double diff2_avg = diff2_sum / (M * N);
+    std::cout << "Average difference: " << diff2_avg * 100.0 << "%" << std::endl;
+    if (diff2_avg > 0.01) {
+      std::cerr << "Too big difference!" << std::endl;
+      return 1;
     }
 
     return 0;
