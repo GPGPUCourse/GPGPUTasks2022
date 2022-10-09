aceb39ac1e81b37cdeb3a340d9618b94f3655b33
borisPristupa <boris.pristupa@gmail.com>
2022-10-09 13:27:25 +0300

    solve task04 (transpose)

--- src/cl/matrix_transpose.cl
+++ src/cl/matrix_transpose.cl
@@ -1,4 +1,97 @@
-__kernel void matrix_transpose(...)
+#define CHUNK_WIDTH 16
+#define CHUNK_HEIGHT 16
+
+__kernel void matrix_transpose(__global const float* src, // m x n
+                               __global       float* dst, // n x m
+                               unsigned int m, unsigned int n)
 {
-    // TODO
+  __local float chunk[CHUNK_WIDTH * CHUNK_HEIGHT];
+  /*
+   src:
+   a b c
+   d e f
+
+   local:
+   a d
+   b e
+   c f
+   */
+
+  int j_src = get_global_id(0);
+  int i_src = get_global_id(1);
+  int j_unraveled = i_src * n + j_src;
+
+  int j_chunk_src = get_local_id(0);
+  int i_chunk_src = get_local_id(1);
+  int j_chunk_unraveled = i_chunk_src * CHUNK_WIDTH + j_chunk_src;
+
+  if (i_src < m && j_src < n) {
+    int j_chunk_local_dst = i_chunk_src;
+    int i_chunk_local_dst = j_chunk_src;
+    chunk[i_chunk_local_dst * CHUNK_HEIGHT + j_chunk_local_dst] = src[j_unraveled];
+  }
+
+  barrier(CLK_LOCAL_MEM_FENCE);
+
+  int j_base_dst = i_src - i_chunk_src;
+  int i_base_dst = j_src - j_chunk_src;
+
+  int j_chunk_dst = j_chunk_unraveled % CHUNK_HEIGHT;
+  int i_chunk_dst = j_chunk_unraveled / CHUNK_HEIGHT;
+
+  int j_dst = j_base_dst + j_chunk_dst;
+  int i_dst = i_base_dst + i_chunk_dst;
+
+  if (i_dst < n && j_dst < m) {
+    dst[i_dst * m + j_dst] = chunk[j_chunk_unraveled];
+  }
+}
+
+#define SKEWED_CHUNK_WIDTH (CHUNK_HEIGHT + CHUNK_WIDTH - 1)
+#define SKEWED_CHUNK_HEIGHT (CHUNK_WIDTH)
+
+__kernel void matrix_transpose_skewed(__global const float* src, // m x n
+                                      __global       float* dst, // n x m
+                                      unsigned int m, unsigned int n)
+{
+  __local float chunk[SKEWED_CHUNK_WIDTH * SKEWED_CHUNK_HEIGHT];
+  /*
+   src:
+   a b c
+   d e f
+
+   local:
+   a d . .
+   . b e .
+   . . c f
+   */
+
+  int j_src = get_global_id(0);
+  int i_src = get_global_id(1);
+  int j_unraveled = i_src * n + j_src;
+
+  int j_chunk_src = get_local_id(0);
+  int i_chunk_src = get_local_id(1);
+  int j_chunk_unraveled = i_chunk_src * CHUNK_WIDTH + j_chunk_src;
+
+  if (i_src < m && j_src < n) {
+    int i_chunk_local_dst = j_chunk_src;
+    int j_chunk_local_dst = i_chunk_src;
+    chunk[i_chunk_local_dst * SKEWED_CHUNK_WIDTH + (j_chunk_local_dst + i_chunk_local_dst)] = src[j_unraveled];
+  }
+
+  barrier(CLK_LOCAL_MEM_FENCE);
+
+  int j_base_dst = i_src - i_chunk_src;
+  int i_base_dst = j_src - j_chunk_src;
+
+  int j_chunk_dst = j_chunk_unraveled % CHUNK_HEIGHT;
+  int i_chunk_dst = j_chunk_unraveled / CHUNK_HEIGHT;
+
+  int j_dst = j_base_dst + j_chunk_dst;
+  int i_dst = i_base_dst + i_chunk_dst;
+
+  if (i_dst < n && j_dst < m) {
+    dst[i_dst * m + j_dst] = chunk[i_chunk_dst * SKEWED_CHUNK_WIDTH + (j_chunk_dst + i_chunk_dst)];
+  }
 }
--- src/main_matrix_transpose.cpp
+++ src/main_matrix_transpose.cpp
@@ -10,71 +10,93 @@
 #include <iostream>
 #include <stdexcept>
 
+template<class Num>
+Num round_up(Num num, Num denom) {
+  return ((num + denom - 1) / denom) * denom;
+}
+
+int run_kernel(std::string kernel_name,
+               unsigned int M, unsigned int K,
+               std::vector<float>& as, std::vector<float>& as_t)
+{
+  ocl::Kernel matrix_transpose_kernel(matrix_transpose, matrix_transpose_length, kernel_name);
+  matrix_transpose_kernel.compile();
+
+  gpu::gpu_mem_32f as_gpu, as_t_gpu;
+  as_gpu.resizeN(M*K);
+  as_t_gpu.resizeN(K*M);
+
+  as_gpu.writeN(as.data(), M*K);
+
+  {
+    unsigned int work_group_size_x = 16;
+    unsigned int work_group_size_y = 16;
+    unsigned int global_work_size_x = round_up(M, work_group_size_x);
+    unsigned int global_work_size_y = round_up(K, work_group_size_y);
+
+    int benchmarkingIters = 10;
+    timer t;
+    for (int iter = 0; iter < benchmarkingIters; ++iter) {
+      // Для этой задачи естественнее использовать двухмерный NDRange. Чтобы это сформулировать
+      // в терминологии библиотеки - нужно вызвать другую вариацию конструктора WorkSize.
+      // В CLion удобно смотреть какие есть вариант аргументов в конструкторах:
+      // поставьте каретку редактирования кода внутри скобок конструктора WorkSize -> Ctrl+P -> заметьте что есть 2, 4 и 6 параметров
+      // - для 1D, 2D и 3D рабочего пространства соответственно
+      matrix_transpose_kernel.exec(
+          gpu::WorkSize(work_group_size_x, work_group_size_y, global_work_size_x, global_work_size_y),
+          as_gpu, as_t_gpu, M, K
+      );
+
+      t.nextLap();
+    }
+    std::cout << "  GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+    std::cout << "  GPU: " << M*K / 1e9 / t.lapAvg() << " GFlops" << std::endl;
+  }
+
+  as_t_gpu.readN(as_t.data(), M*K);
+
+  // Проверяем корректность результатов
+  for (int j = 0; j < M; ++j) {
+    for (int i = 0; i < K; ++i) {
+      float a = as[j * K + i];
+      float b = as_t[i * M + j];
+      if (a != b) {
+        std::cerr << "Not the same! " << std::endl;
+        return 1;
+      }
+    }
+  }
+  return 0;
+}
 
 int main(int argc, char **argv)
 {
-    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+  gpu::Device device = gpu::chooseGPUDevice(argc, argv);
 
-    gpu::Context context;
-    context.init(device.device_id_opencl);
-    context.activate();
+  gpu::Context context;
+  context.init(device.device_id_opencl);
+  context.activate();
 
-    int benchmarkingIters = 10;
-    unsigned int M = 1024;
-    unsigned int K = 1024;
+  unsigned int M = 8192;
+  unsigned int K = 8192;
 
-    std::vector<float> as(M*K, 0);
-    std::vector<float> as_t(M*K, 0);
+  std::vector<float> as(M*K, 0);
+  std::vector<float> as_t(M*K, 0);
 
-    FastRandom r(M+K);
-    for (unsigned int i = 0; i < as.size(); ++i) {
-        as[i] = r.nextf();
-    }
-    std::cout << "Data generated for M=" << M << ", K=" << K << "!" << std::endl;
+  FastRandom r(M+K);
+  for (unsigned int i = 0; i < as.size(); ++i) {
+    as[i] = r.nextf();
+  }
+  std::cout << "Data generated for M=" << M << ", K=" << K << "!" << std::endl;
 
-    /*
-    gpu::gpu_mem_32f as_gpu, as_t_gpu;
-    as_gpu.resizeN(M*K);
-    as_t_gpu.resizeN(K*M);
+  std::cout << "Regular transpose:" << std::endl;
+  if (run_kernel("matrix_transpose", M, K, as, as_t)) {
+    return 1;
+  }
+  std::cout << "Skewed transpose:" << std::endl;
+  if (run_kernel("matrix_transpose_skewed", M, K, as, as_t)) {
+    return 1;
+  }
 
-    as_gpu.writeN(as.data(), M*K);
-
-    ocl::Kernel matrix_transpose_kernel(matrix_transpose, matrix_transpose_length, "matrix_transpose");
-    matrix_transpose_kernel.compile();
-
-    {
-        timer t;
-        for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
-            // Для этой задачи естественнее использовать двухмерный NDRange. Чтобы это сформулировать
-            // в терминологии библиотеки - нужно вызвать другую вариацию конструктора WorkSize.
-            // В CLion удобно смотреть какие есть вариант аргументов в конструкторах:
-            // поставьте каретку редактирования кода внутри скобок конструктора WorkSize -> Ctrl+P -> заметьте что есть 2, 4 и 6 параметров
-            // - для 1D, 2D и 3D рабочего пространства соответственно
-            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, as_t_gpu, M, K);
-
-            t.nextLap();
-        }
-        std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << M*K/1000.0/1000.0 / t.lapAvg() << " millions/s" << std::endl;
-    }
-
-    as_t_gpu.readN(as_t.data(), M*K);
-
-    // Проверяем корректность результатов
-    for (int j = 0; j < M; ++j) {
-        for (int i = 0; i < K; ++i) {
-            float a = as[j * K + i];
-            float b = as_t[i * M + j];
-            if (a != b) {
-                std::cerr << "Not the same!" << std::endl;
-                return 1;
-            }
-        }
-    }
-    */
-
-    return 0;
+  return 0;
 }
