0562a980344baa2880f3e26c65c45c3bf74fbee4
borisPristupa <boris.pristupa@gmail.com>
2022-10-18 12:39:33 +0300

    reimplement with per-item merge path solving

--- src/main_merge.cpp
+++ src/main_merge.cpp
@@ -40,11 +40,11 @@ int main(int argc, char **argv) {
     context.activate();
 
     int benchmarkingIters = 10;
-    unsigned int n = 32 * 1024 * 1024; // todo
+    unsigned int n = 32 * 1024 * 1024;
     std::vector<float> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
-        as[i] = ((int) r.nextf() % 5) + 5;
+        as[i] = r.nextf();
     }
     std::cout << "Data generated for n=" << n << "!" << std::endl;
 
@@ -61,33 +61,26 @@ int main(int argc, char **argv) {
     }
 
     gpu::gpu_mem_32f as_gpu;
+    gpu::gpu_mem_32f result_gpu;
     as_gpu.resizeN(n);
+    result_gpu.resizeN(n);
     {
-        ocl::Kernel merge_global(merge_kernel, merge_kernel_length, "merge_global");
-        ocl::Kernel merge_path_global(merge_kernel, merge_kernel_length, "merge_path_global");
-        ocl::Kernel merge_local(merge_kernel, merge_kernel_length, "merge_local");
-        merge_global.compile();
-        merge_path_global.compile();
-        merge_local.compile();
+        ocl::Kernel merge(merge_kernel, merge_kernel_length, "merge");
+        merge.compile();
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             as_gpu.writeN(as.data(), n);
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфера данных
             unsigned int workGroupSize = 128;
-            unsigned int locallySortable = 3;
-            unsigned int global_work_size = round_up(div_up(n, 2 * locallySortable), workGroupSize);
-            merge_local.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n);
-
-            for (unsigned int merged_src_len = locallySortable * 2;
-                 merged_src_len < n; merged_src_len *= 2)
-            {
-              merge_path_global.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n, merged_src_len);
-              merge_global.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n);
+            unsigned int global_work_size = round_up(n, workGroupSize);
+            for (int merged_src_len = 1; merged_src_len < n; merged_src_len *= 2) {
+              merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, result_gpu, n, merged_src_len);
+              std::swap(as_gpu, result_gpu);
             }
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "GPU: " << (n / 1000.0 / 1000) / t.lapAvg() << " millions/s" << std::endl;
         as_gpu.readN(as.data(), n);
     }
     // Проверяем корректность результатов
