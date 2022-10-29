aebf632d808802106f94be7b1e929252a99c1b2d
df530 <fild10@yandex.ru>
2022-10-30 01:38:40 +0300

    Implement prefix sum

--- src/cl/prefix_sum.cl
+++ src/cl/prefix_sum.cl
@@ -1,1 +1,26 @@
-// TODO
+#ifdef __CLION_IDE__
+
+#include <libgpu/opencl/cl/clion_defines.cl>
+
+#endif
+
+#line 6
+
+__kernel void prefix_sum(__global unsigned int *cur_prefix_sum, __global unsigned int *cur_part_sum,
+                         unsigned int cur_bit, unsigned int n) {
+    unsigned int global_i = get_global_id(0);
+    if (global_i >= n)
+        return;
+    if (((global_i + 1) >> cur_bit) & 1) {
+        unsigned int part_sum_id = ((global_i + 1) >> cur_bit) - 1;
+        cur_prefix_sum[global_i] += cur_part_sum[part_sum_id];
+    }
+}
+
+__kernel void part_sum(__global unsigned int *cur_part_sum, __global unsigned int *next_part_sum,
+                       unsigned int numOfNextParts) {
+    unsigned int global_i = get_global_id(0);
+    if (global_i >= numOfNextParts)
+        return;
+    next_part_sum[global_i] = cur_part_sum[global_i * 2] + cur_part_sum[global_i * 2 + 1];
+}
--- src/main_prefix_sum.cpp
+++ src/main_prefix_sum.cpp
@@ -76,8 +76,59 @@ int main(int argc, char **argv)
 			std::cout << "CPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
 		}
 
-		{
-			// TODO: implement on OpenCL
+        {
+            std::vector<unsigned int> array_of_zeros(n, 0);
+            std::vector<unsigned int> result(n);
+
+            gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+
+            gpu::Context context;
+            context.init(device.device_id_opencl);
+            context.activate();
+
+
+            gpu::gpu_mem_32u cur_prefix_sum_gpu;
+            cur_prefix_sum_gpu.resizeN(n);
+
+            gpu::gpu_mem_32u cur_part_sum_gpu;
+            cur_part_sum_gpu.resizeN(n);
+            gpu::gpu_mem_32u next_part_sum_gpu;
+            next_part_sum_gpu.resizeN(n);
+            
+            ocl::Kernel prefix_sum(prefix_sum_kernel, prefix_sum_kernel_length, "prefix_sum");
+            prefix_sum.compile();
+            ocl::Kernel part_sum(prefix_sum_kernel, prefix_sum_kernel_length, "part_sum");
+            part_sum.compile();
+
+            timer t;
+            for (int iter = 0; iter < benchmarkingIters; ++iter) {
+                cur_prefix_sum_gpu.writeN(array_of_zeros.data(), n);
+                cur_part_sum_gpu.writeN(as.data(), n);
+
+                t.restart();
+                unsigned int work_group_size = 128;
+                unsigned int prefix_sum_global_work_size = (n + work_group_size - 1) / work_group_size * work_group_size;
+                unsigned int num_of_next_parts = n / 2;
+                unsigned int part_sum_global_work_size = (num_of_next_parts + work_group_size - 1) / work_group_size * work_group_size;
+                for (int cur_bit = 0; (1 << cur_bit) <= n; cur_bit++) {
+                    prefix_sum.exec(gpu::WorkSize(work_group_size, prefix_sum_global_work_size),
+                                    cur_prefix_sum_gpu, cur_part_sum_gpu, cur_bit, n);
+
+                    part_sum.exec(gpu::WorkSize(work_group_size, part_sum_global_work_size),
+                                  cur_part_sum_gpu, next_part_sum_gpu, num_of_next_parts);
+
+                    cur_part_sum_gpu.swap(next_part_sum_gpu);
+                    num_of_next_parts /= 2;
+                }
+                t.nextLap();
+
+                cur_prefix_sum_gpu.readN(result.data(), n);
+                for (int i = 0; i < n; ++i) {
+                    EXPECT_THE_SAME(reference_result[i], result[i], "GPU result should be consistent!");
+                }
+            }
+            std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
 		}
 	}
 }
