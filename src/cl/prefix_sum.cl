edf9fd0961944500c97e4db758f576995e346433
Raed Romanov <raid_r@mail.ru>
2022-11-01 00:54:41 +0300

    Task 5.2 done

--- src/cl/prefix_sum.cl
+++ src/cl/prefix_sum.cl
@@ -1,1 +1,20 @@
-// TODO
+__kernel void psum_fill_zero(__global uint* as, uint n) {
+    size_t id = get_global_id(0);
+    if (id < n) {
+        as[id] = 0;
+    }
+}
+
+__kernel void psum_reduce_adjacent(__global uint* in, __global uint* out, uint n) {
+    size_t id = get_global_id(0);
+    if (id * 2 < n) {
+        out[id] = in[id * 2] + ((id * 2 + 1 < n) ? in[id * 2 + 1] : 0);
+    }
+}
+
+__kernel void psum_reduce(__global uint* as, __global uint* bs, uint n, uint step) {
+    size_t id = get_global_id(0);
+    if (id < n && ((id + 1) & ((size_t)1 << step))) {
+        bs[id] += as[((id + 1) >> step) - 1];
+    }
+}
--- src/main_prefix_sum.cpp
+++ src/main_prefix_sum.cpp
@@ -19,11 +19,36 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+template <class T>
+std::ostream& operator <<(std::ostream& s, const std::vector<T>& data) {
+    for (const T& x : data) {
+        s << x << ' ';
+    }
+    return s;
+}
 
 int main(int argc, char **argv)
 {
 	int benchmarkingIters = 10;
 	unsigned int max_n = (1 << 24);
+
+    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+
+    gpu::Context context;
+    context.init(device.device_id_opencl);
+    context.activate();
+
+    gpu::gpu_mem_32u as_gpu, bs_gpu, cs_gpu;
+    as_gpu.resizeN(max_n);
+    bs_gpu.resizeN(max_n);
+    cs_gpu.resizeN(max_n);
+
+    ocl::Kernel kernelReduceAdjacent(prefix_sum_kernel, prefix_sum_kernel_length, "psum_reduce_adjacent");
+    kernelReduceAdjacent.compile();
+    ocl::Kernel kernelReduce(prefix_sum_kernel, prefix_sum_kernel_length, "psum_reduce");
+    kernelReduce.compile();
+    ocl::Kernel kernelFillZero(prefix_sum_kernel, prefix_sum_kernel_length, "psum_fill_zero");
+    kernelFillZero.compile();
 
 	for (unsigned int n = 2; n <= max_n; n *= 2) {
 		std::cout << "______________________________________________" << std::endl;
@@ -77,7 +102,42 @@ int main(int argc, char **argv)
 		}
 
 		{
-			// TODO: implement on OpenCL
+            size_t workGroupSize = 128;
+            size_t workSize = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+            gpu::WorkSize ws(workGroupSize, workSize);
+
+            timer t;
+            for (size_t i = 0; i < benchmarkingIters; ++i) {
+                as_gpu.writeN(as.data(), n);
+
+                t.restart();
+
+                for (uint32_t step = 0; (1 << step) < 2 * n; ++step) {
+                    if (step) {
+                        uint32_t m = n >> (step - 1);
+                        size_t reduceAdjacentWorkSize =
+                                ((m + 1) / 2 + workGroupSize - 1) / workGroupSize * workGroupSize;
+                        kernelReduceAdjacent.exec(gpu::WorkSize(workGroupSize, reduceAdjacentWorkSize),
+                                                  as_gpu, cs_gpu, m);
+                        std::swap(as_gpu, cs_gpu);
+                    } else {
+                        kernelFillZero.exec(ws, bs_gpu, n);
+                    }
+                    kernelReduce.exec(ws, as_gpu, bs_gpu, n, step);
+                }
+
+                t.nextLap();
+            }
+
+            std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
+
+            std::vector<unsigned int> result(n);
+            bs_gpu.readN(result.data(), n);
+
+            for (int i = 0; i < n; ++i) {
+                EXPECT_THE_SAME(reference_result[i], result[i], "GPU result should be consistent!");
+            }
 		}
 	}
 }
