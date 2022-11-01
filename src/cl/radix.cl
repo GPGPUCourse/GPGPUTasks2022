981721f914d06d149e916dfa706eab4f4b73cb43
Rustam Sadykov <sbonekenobi@gmail.com>
2022-11-01 03:56:57 +0300

    make 6.3

--- src/cl/radix.cl
+++ src/cl/radix.cl
@@ -1,3 +1,107 @@
-__kernel void radix(__global unsigned int *as) {
-    // TODO
+#ifdef __CLION_IDE__
+
+#include <libgpu/opencl/cl/clion_defines.cl>
+
+#endif
+
+#line 6
+
+#ifndef WORK_GROUP_SIZE
+#define WORK_GROUP_SIZE 128
+#endif
+
+#ifndef BATCH_SIZE
+#define BATCH_SIZE 4
+#endif
+
+#define BATCH_COUNT (1 << BATCH_SIZE)
+#define MASK (BATCH_COUNT - 1)
+
+#define get_type(x, bit) ((x >> bit) & MASK)
+
+#define get_groups(x) ((x + WORK_GROUP_SIZE - 1) / WORK_GROUP_SIZE)
+
+__kernel void count(
+    __global unsigned int *as,
+    __global unsigned int *hist,
+    unsigned int n,
+    unsigned int bit
+) {
+    const unsigned int global_id = get_global_id(0);
+    const unsigned int local_id = get_local_id(0);
+    const unsigned int group_id = get_group_id(0);
+    const unsigned int groups = get_groups(n);
+
+    __local unsigned int local_hist[BATCH_COUNT * WORK_GROUP_SIZE];
+    for (unsigned int i = 0; i < BATCH_COUNT; ++i) {
+        local_hist[i * WORK_GROUP_SIZE + local_id] = 0;
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (global_id < n) {
+        unsigned int type = get_type(as[global_id], bit);
+        local_hist[local_id * BATCH_COUNT + type] = 1;
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (local_id < BATCH_COUNT) {
+        unsigned int sum = 0;
+        for (unsigned int i = 0; i < WORK_GROUP_SIZE; ++i) {
+            sum += local_hist[i * BATCH_COUNT + local_id];
+        }
+
+        hist[local_id * groups + group_id] = sum;
+    }
+
+}
+
+__kernel void radix(
+    __global unsigned int *as,
+    __global unsigned int *bs,
+    unsigned int n,
+    __global unsigned int *hist,
+    unsigned int bit
+) {
+    const unsigned int global_id = get_global_id(0);
+    const unsigned int local_id = get_local_id(0);
+    const unsigned int group_id = get_group_id(0);
+    const unsigned int groups = get_groups(n);
+
+    __local unsigned int uploaded[WORK_GROUP_SIZE];
+    __local unsigned int local_hist[BATCH_COUNT * WORK_GROUP_SIZE];
+
+    for (unsigned int i = 0; i < BATCH_COUNT; ++i) {
+        local_hist[i * WORK_GROUP_SIZE + local_id] = 0;
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (global_id < n) {
+        unsigned int x = as[global_id];
+        unsigned int type = get_type(x, bit);
+        uploaded[local_id] = x;
+        local_hist[type * WORK_GROUP_SIZE + local_id] = 1;
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (local_id < BATCH_COUNT) {
+        unsigned int sum = 0;
+        for (int i = 0; i < WORK_GROUP_SIZE; ++i) {
+            unsigned int idx = local_id * WORK_GROUP_SIZE + i;
+            unsigned int tmp = local_hist[idx];
+            local_hist[idx] = sum;
+            sum += tmp;
+        }
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (global_id < n) {
+        unsigned int x = uploaded[local_id];
+        unsigned int type = get_type(x, bit);
+        unsigned int hist_idx = type * groups + group_id;
+        unsigned int new_idx = (hist_idx == 0 ? 0 : hist[hist_idx - 1]) + local_hist[type * WORK_GROUP_SIZE + local_id];
+        bs[new_idx] = x;
+    }
+
 }
--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -6,6 +6,7 @@
 
 // Этот файл будет сгенерирован автоматически в момент сборки - см. convertIntoHeader в CMakeLists.txt:18
 #include "cl/radix_cl.h"
+#include "cl/prefix_sum_cl.h"
 
 #include <iostream>
 #include <stdexcept>
@@ -50,13 +51,39 @@ int main(int argc, char **argv) {
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
-    gpu::gpu_mem_32u as_gpu;
+
+    unsigned int workGroupSize = 128;
+    unsigned int groups = (n + workGroupSize - 1) / workGroupSize;
+    unsigned int batchSize = 4;
+    unsigned int batchCount = 1 << batchSize;
+    unsigned int histSize = groups * batchCount;
+
+    gpu::gpu_mem_32u as_gpu, hist_gpu, hist_pref_gpu, hist_buf_gpu, bs_gpu;
     as_gpu.resizeN(n);
+    bs_gpu.resizeN(n);
+    hist_gpu.resizeN(histSize);
+    hist_pref_gpu.resizeN(histSize);
+    hist_buf_gpu.resizeN(histSize);
 
     {
-        ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix");
+
+        std::string defines = " -D WORK_GROUP_SIZE=" + to_string(workGroupSize);
+        defines += " -D BATCH_SIZE=" + to_string(batchSize);
+
+        ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix", defines);
         radix.compile();
+
+        ocl::Kernel count(radix_kernel, radix_kernel_length, "count", defines);
+        count.compile();
+
+        ocl::Kernel init(prefix_sum_kernel, prefix_sum_kernel_length, "init");
+        init.compile();
+
+        ocl::Kernel update(prefix_sum_kernel, prefix_sum_kernel_length, "update");
+        update.compile();
+
+        ocl::Kernel reduce(prefix_sum_kernel, prefix_sum_kernel_length, "reduce");
+        reduce.compile();
 
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
@@ -64,9 +91,30 @@ int main(int argc, char **argv) {
 
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
 
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            radix.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n);
+            unsigned int global_work_size = groups * workGroupSize;
+
+            for (unsigned int bit = 0; bit < sizeof(unsigned int) * CHAR_BIT; bit += batchSize) {
+                count.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, hist_gpu, n, bit);
+
+                { // pref sum
+                    unsigned int fullSize = (histSize + workGroupSize - 1) / workGroupSize * workGroupSize;
+                    init.exec(gpu::WorkSize(workGroupSize, fullSize), hist_pref_gpu, histSize);
+                    unsigned int m = histSize;
+                    auto reduceWorkSize = [&]() {
+                        return ((m + 1) / 2 + workGroupSize - 1) / workGroupSize * workGroupSize;
+                    };
+                    for (unsigned int pref_bit = 0; (1 << pref_bit) <= histSize; ++pref_bit) {
+                        update.exec(gpu::WorkSize(workGroupSize, fullSize), hist_gpu, hist_pref_gpu, histSize, pref_bit);
+                        reduce.exec(gpu::WorkSize(workGroupSize, reduceWorkSize()), hist_gpu, hist_buf_gpu, m);
+                        m = (m + 1) / 2;
+                        std::swap(hist_gpu, hist_buf_gpu);
+                    }
+                }
+
+                radix.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, bs_gpu, n, hist_pref_gpu, bit);
+                std::swap(as_gpu, bs_gpu);
+            }
+
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
@@ -79,6 +127,6 @@ int main(int argc, char **argv) {
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
